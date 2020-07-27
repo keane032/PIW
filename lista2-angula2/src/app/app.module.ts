38566656c55c83f0b868bfc10841bcb5c59e48d1
keane032 <keane032@gmail.com>
2020-07-27 19:40:16 -0300

    add lista 2

---
+++ lista2-angula2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PostsComponent } from './posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavegadorComponent,
+    LayoutComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
